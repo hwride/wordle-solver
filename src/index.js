2730e120f4d1921fddc599d8adf386c8ec73ee31
Hugo Wride <hugowride@gmail.com>
2022-01-27 22:08:58 +0000

    Refactoring.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
- const { createGuesser } = require('./guesser')
+const { createGuesser } = require('./guesser')
+const { wait } = require('./utils')
 
 const {
   delayBeforeCloseModalMs,
@@ -14,108 +15,112 @@ const {
 main()
 
 async function main() {
-  var gameApp = document.querySelector('game-app')
-  var gameThemeManager = gameApp.shadowRoot.querySelector('game-theme-manager')
-  var gameRows = gameThemeManager.querySelectorAll('game-row')
-
-  const guesser = createGuesser()
-
-  await closeModalIfOpen(gameThemeManager)
-
+  // Some page init stuff.
+  await closeModalIfOpen()
   await wait(delayBeforeStartMs)
 
+  // Start making guesses.
+  const guesser = createGuesser()
   await makeGuesses(guesser)
-  async function closeModalIfOpen(gameThemeManager) {
-    if(gameThemeManager.querySelector('game-modal').hasAttribute('open')) {
-      await wait(delayBeforeCloseModalMs)
-      gameThemeManager.querySelector('game-modal').removeAttribute('open')
+}
+
+async function closeModalIfOpen() {
+  const gameModal = getGameThemeManager().querySelector('game-modal')
+  if (gameModal.hasAttribute('open')) {
+    await wait(delayBeforeCloseModalMs)
+    gameModal.removeAttribute('open')
+  }
+}
+
+async function makeGuesses(guesser) {
+  let success = false
+  let rowIndex = 0
+  while (rowIndex < numberOfGuesses && !success) {
+    success = await makeGuess(guesser, rowIndex)
+    rowIndex++
+  }
+}
+
+async function makeGuess(guesser, rowIndex) {
+  let isGuessValidBool = false
+  while (!isGuessValidBool) {
+    const guessWord = guesser.findNextGuess()
+    await enterGuess(guessWord)
+    await submitGuess()
+    if (isGuessValid(rowIndex)) {
+      isGuessValidBool = true
+    } else {
+      // Clear existing word and guess again.
+      await wait(delayAfterInvalidWordMs) // Make it look like it's stopping to think
+      await clearGuess()
+      guesser.registerInvalidWord(guessWord)
     }
   }
-
-  async function makeGuesses(guesser) {
-    let success = false
-    let rowIndex = 0
-    while(rowIndex < numberOfGuesses && !success) {
-      success = await makeGuess(guesser, rowIndex)
-      rowIndex++
-    }
+  await waitForRowGuessToBeReady(rowIndex)
+  const isSuccessBool = isSuccess(rowIndex)
+  if (!isSuccessBool) {
+    const guessResult = getRowWord(rowIndex)
+    guesser.modifyConfigFromGuess(guessResult)
   }
+  return isSuccessBool
+}
 
-  async function makeGuess(guesser, rowIndex) {
-    let isGuessValidBool = false
-    while(!isGuessValidBool) {
-      const guessWord = guesser.findNextGuess()
-      await enterGuess(guessWord)
-      await submitGuess()
-      if(isGuessValid(rowIndex)) {
-        isGuessValidBool = true
-      } else {
-        // Clear existing word and guess again.
-        await wait(delayAfterInvalidWordMs) // Make it look like it's stopping to think
-        await clearGuess()
-        guesser.registerInvalidWord(guessWord)
-      }
-    }
-    await waitForRowGuessToBeReady(rowIndex)
-    const isSuccessBool = isSuccess(rowIndex)
-    if(!isSuccessBool) {
-      const guessResult = getRowWord(rowIndex)
-      guesser.modifyConfigFromGuess(guessResult)
-    }
-    return isSuccessBool
-  }
+async function waitForRowGuessToBeReady() {
+  return await wait(delayWaitForGuessToBeReadyMs)
+}
 
-  async function waitForRowGuessToBeReady() {
-    return await wait(delayWaitForGuessToBeReadyMs)
-  }
+function isGuessValid(rowIndex) {
+  const isInvalid = getGameRow(rowIndex).hasAttribute('invalid')
+  return !isInvalid
+}
 
-  function isGuessValid(rowIndex) {
-    const isInvalid = gameRows[rowIndex].hasAttribute('invalid')
-    return !isInvalid
-  }
+function isSuccess(rowIndex) {
+  return [...getRowTiles(rowIndex)].every(tile => tile.attributes.evaluation.value === 'correct')
+}
 
-  function isSuccess(rowIndex) {
-    return [...getRowTiles(rowIndex)].every(tile => tile.attributes.evaluation.value === 'correct')
+async function enterGuess(word) {
+  for (const letter of word.split('')) {
+    enterLetter(letter)
+    await wait(delayBetweenEnterLetterMs)
   }
+}
 
-  async function enterGuess(word) {
-    for(const letter of word.split('')) {
-      enterLetter(letter)
-      await wait(delayBetweenEnterLetterMs)
-    }
-  }
+async function submitGuess() {
+  enterLetter('Enter')
+}
 
-  async function submitGuess() {
-    enterLetter('Enter')
+async function clearGuess() {
+  for (let i = 0; i < numberOfLetters; i++) {
+    enterLetter('Backspace')
+    await wait(delayBetweenDeleteLetterMs)
   }
+}
 
-  async function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
+function enterLetter(letter) {
+  getGameThemeManager().querySelector('#game').dispatchEvent(new CustomEvent("game-key-press", {
+    detail: { key: letter }
+  }));
+}
 
-  async function clearGuess() {
-    for(let i = 0; i < numberOfLetters; i++) {
-      enterLetter('Backspace')
-      await wait(delayBetweenDeleteLetterMs)
-    }
-  }
+function getRowWord(rowIndex) {
+  const tiles = getRowTiles(rowIndex)
+  const rowLetters = [...tiles].map(tile => ({
+    letter: tile.attributes.letter.value,
+    evaluation: tile.attributes.evaluation.value,
+  }))
+  return rowLetters
+}
 
-  function enterLetter(letter) {
-    gameThemeManager.querySelector('#game').dispatchEvent(new CustomEvent("game-key-press", {
-      detail: { key: letter }
-    }));
-  }
+function getRowTiles(rowIndex) {
+  return getGameRow(rowIndex).shadowRoot.querySelectorAll('game-tile')
+}
 
-  function getRowWord(rowIndex) {
-    const tiles = getRowTiles(rowIndex)
-    const rowLetters = [...tiles].map(tile => ({
-      letter: tile.attributes.letter.value,
-      evaluation: tile.attributes.evaluation.value,
-    }))
-    return rowLetters
-  }
+function getGameThemeManager() {
+  return document
+    .querySelector('game-app').shadowRoot
+    .querySelector('game-theme-manager')
+}
 
-  function getRowTiles(rowIndex) {
-    return gameRows[rowIndex].shadowRoot.querySelectorAll('game-tile')
-  }
+function getGameRow(i) {
+  return getGameThemeManager().querySelectorAll('game-row')[i]
 }
