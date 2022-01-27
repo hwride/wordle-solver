8ba2fe790dc41c0ac38156f121b6c748943b020d
Hugo Wride <hugowride@gmail.com>
2022-01-27 20:11:41 +0000

    Change extension to execute some script on popup button click.

---
+++ popup.js
@@ -0,0 +1,15 @@
+// When the button is clicked trigger the solve Wordle code.
+document.getElementById("solveWordle").addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  console.log('Solve that Wordle!')
+}
